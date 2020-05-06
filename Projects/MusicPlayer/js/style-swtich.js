96c7062c51d39fea13e5011273eea3cb5b8c80b3
vidnas <59883737+vidnas@users.noreply.github.com>
2020-05-06 11:45:03 +0100

    Add files via upload

---
+++ Projects/MusicPlayer/js/style-swtich.js
@@ -0,0 +1,7 @@
+function styleSwapDark() {
+    document.getElementById("css-sheet").href = "./css/dark.css";
+}
+
+function styleSwapLight() {
+    document.getElementById("css-sheet").href = "./css/light.css";
+}
