0e39d31fd02632c1d0ada157d18ac2b8fc0d95ab
vidnas <59883737+vidnas@users.noreply.github.com>
2020-05-27 12:11:56 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,19 +5,13 @@ function replace(hide0, hide1, hide2, show) {
   document.getElementById(show).style.display= "block";
 }
 
-// Get the modal
 var modal = document.getElementById("form-modal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("modal-btn");
 
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
